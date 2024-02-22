fe75d2e6965d76b75812935cfe6a8658105505d6
WangYiran01 <129300550+WangYiran01@users.noreply.github.com>
2024-02-22 21:37:26 +0800

    Update Test02.lean

--- Test02.lean
+++ Test02.lean
@@ -1,5 +1,10 @@
-open Nat (add_assoc add_comm)
+import Mathlib.Data.Nat.Choose.Basic
+import Mathlib.Tactic.Linarith
 
-theorem hello_world (a b c : Nat)
-  : a + b + c = a + c + b := by
-  rw [add_assoc, add_comm b, ←add_assoc]
+open Nat
+
+theorem choose_mul_eq_mul_sub {n k : ℕ} (hkn : k ≤ n) (hsk : 1 ≤ k) :  -- 定理1.3
+    choose n k * k  = n * choose (n - 1) (k - 1) := by
+      have choose_mul_eq_choose_mul : choose n k * choose k 1 = choose n 1 * choose (n - 1) (k - 1)  := by rw[choose_mul hkn hsk]
+      rw[choose_one_right, choose_one_right] at choose_mul_eq_choose_mul
+      rw[choose_mul_eq_choose_mul]
